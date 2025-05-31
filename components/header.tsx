10fc97558a8cc9bf032bd97e539dc72538257f4e
Arjun Kundu <kunduarjun02@gmail.com>
2025-05-31 18:44:47 -0300

    Beginning header.tsx, Timestamp: 54:09

---
+++ components/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className="z-[999] relative">
+        <div>
+            
+        </div>
+    </header>
+  )
+}
