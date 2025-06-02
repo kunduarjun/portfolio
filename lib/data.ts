8dba8d9f7adb4d9459c79b9a55bcb9dd0409b1ae
Arjun Kundu <kunduarjun02@gmail.com>
2025-06-01 21:16:57 -0300

    Added the initial header styling and changed favicon

--- lib/data.ts
+++ lib/data.ts
@@ -3,3 +3,39 @@ import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
 
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+] as const;
+
+export const projectsData = [
+] as const;
+
+export const skillsData = [
+] as const;
+
