78bb82e5f0fb4cf4f3d7dbd78c3f194a9625483d
Arjun Kundu <kunduarjun02@gmail.com>
2025-06-02 13:34:54 -0300

    Added spinning effect

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,15 +1,18 @@
+'use client';
+
 import React from 'react'
 import Image from "next/image";
 import Headshot from '@/public/Headshot.jpeg';
+import { motion } from 'framer-motion';
 
 export default function Intro() {
   return (
     <section>
         <div className="flex items-center justify-center">
             <div className="relative">
-                <Image 
-                    src={Headshot} alt="Arjun's Headshot" width="192" height="192" quality="95" priority={true} className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
-                    />
+                <motion.div initial={{ rotate: 0 }} animate={{ rotate: [0, 360, 0] }} transition={{ duration: 2, ease: "easeInOut", times: [0, 0.5, 1] }} className="h-24 w-24">
+                    <Image src={Headshot} alt="Arjun's Headshot" width="192" height="192" quality="95" priority={true} className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"/>
+                </motion.div>
                 <span className="absolute text-5xl -top-7 left-3">
                     🫳
                 </span>
