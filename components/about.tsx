39f614ad8a10b3880eeb64cba10b39b3ddc04664
Arjun Kundu <kunduarjun02@gmail.com>
2025-06-03 23:08:16 -0300

    Began 'About' section. Need to add paragraph text

---
+++ components/about.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <section className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40">
+        <h2 className="text-3xl font-medium capitalize mb-8">About me</h2>
+        <p className="mb-3"></p>
+        <p></p>
+    </section>
+  )
+}
--- components/intro.tsx
+++ components/intro.tsx
@@ -48,12 +48,12 @@ export default function Intro() {
         </div>
       </div>
 
-      <motion.p className="mb-10 mt-5 px-4 text-xl font-medium !leading-[1.5] sm:text-2xl" initial={{ opacity: 0, y: 100 }} animate={{opacity: 1, y: 0 }}>
+      <motion.h1 className="mb-10 mt-5 px-4 text-xl font-medium !leading-[1.5] sm:text-2xl" initial={{ opacity: 0, y: 100 }} animate={{opacity: 1, y: 0 }}>
         <span className="font-bold">{`Welcome to the portfolio of Arjun Kundu 👋`}</span> {`I'm a `}
         <span className="font-bold">{`Python Developer`}</span> {`and`} <span className="font-bold">{`DevOps Analyst`}</span> {`with `}
         <span className="font-bold">{`5 years`}</span> {`of experience. `} 
         {`I'm currently seeking `} <span className="underline">{`Development-related`}</span> {` and `} <span className="underline">{`DevOps-related`}</span> {` opportunities.`}
-      </motion.p>
+      </motion.h1>
 
       <motion.div className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium" initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 0.3 }}>
         <Link href="#contact" className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition">Contact me ✉️ <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" /></Link>
