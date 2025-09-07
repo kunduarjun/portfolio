c33554a890eb088bc6ad9087e37efb379f36f9b1
Arjun Kundu <kunduarjun02@gmail.com>
2025-09-06 22:31:09 -0300

    Finished Contact section

---
+++ components/contact.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import React from 'react'
+import SectionHeading from './section-heading'
+import { motion } from 'framer-motion'
+import { useSectionInView } from '@/lib/hooks';
+import { sendEmail } from '@/actions/sendEmail';
+import SubmitButton from './submit-button';
+import toast from 'react-hot-toast';
+
+export default function Contact() {
+  const { ref } = useSectionInView("Contact");
+
+  return (
+    <motion.section ref={ref} id="contact" className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center" initial={{ opacity: 0, }} whileInView={{ opacity: 1, }} transition={{ duration: 1, }} viewport={{ once: true, }}>
+        <SectionHeading>Contact me</SectionHeading>
+        <p className="text-gray-700 -mt-4">Please contact me directly at <a className="underline" href="mailto:kunduarjun02@gmail.com">kunduarjun02@gmail.com</a> or through this form.</p>
+        <form className="mt-10 flex flex-col" action={async formData => {
+          const { data, error } = await sendEmail(formData);
+          if (error) { toast.error(error); return; }
+          toast.success("Message sent successfully!");
+        }}>
+            <input className="h-14 rounded-lg border border-black/10 px-4" required maxLength={500} name="senderEmail" type="email" placeholder="Your email" />
+            <textarea className="h-52 my-3 rounded-lg border border-black/10 p-4" required maxLength={5000} name="message" placeholder="Your message" />
+            <SubmitButton />
+        </form>
+    </motion.section>
+  )
+}
