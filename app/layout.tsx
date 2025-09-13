b7c8fc568f86e95adb0ee1b3fc127fb8a75a94be
Arjun Kundu <kunduarjun02@gmail.com>
2025-09-13 19:57:49 -0400

    Adding option for dark mode...

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import Header from '@/components/header';
 import ActiveSectionContextProvider from '@/context/active-section-context';
 import { Toaster } from 'react-hot-toast';
 import Footer from '@/components/footer';
+import ThemeSwitch from '@/components/theme-switch';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,7 +20,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="!scroll-smooth">
-      <body className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36`}>
+      <body className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36
+      dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}>
         <ActiveSectionContextProvider>
           <Header />
           {children}
@@ -27,6 +29,8 @@ export default function RootLayout({
 
           <Toaster position="top-right" />
         </ActiveSectionContextProvider>
+
+        <ThemeSwitch />
       </body>
     </html>
   )
