2504af20fff683cf61e1a41f199e7c83dab1cb90
Arjun Kundu <kunduarjun02@gmail.com>
2025-09-13 20:18:55 -0400

    Added emoji to the contact title

--- components/contact.tsx
+++ components/contact.tsx
@@ -13,7 +13,7 @@ export default function Contact() {
 
   return (
     <motion.section ref={ref} id="contact" className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center" initial={{ opacity: 0, }} whileInView={{ opacity: 1, }} transition={{ duration: 1, }} viewport={{ once: true, }}>
-        <SectionHeading>Contact me</SectionHeading>
+        <SectionHeading>Contact me 🔔</SectionHeading>
         <p className="text-gray-700 -mt-4">Please contact me directly at <a className="underline" href="mailto:kunduarjun02@gmail.com">kunduarjun02@gmail.com</a> or through this form.</p>
         <form className="mt-10 flex flex-col" action={async formData => {
           const { data, error } = await sendEmail(formData);
